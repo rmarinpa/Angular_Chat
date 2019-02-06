4c13647fc8e32ec07b359670f2c6170542762b48
Ricardo Marin <ricardomarinpalacios@gmail.com>
2019-02-06 09:43:38 -0300

    update

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { ChatComponent } from './components/chat/chat.component';
+import {FormsModule} from '@angular/forms';
+
+//Servicios
+
+import {ChatService} from "./providers/chat.service";
+import { LoginComponent } from './components/login/login.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    FormsModule
+  ],
+  providers: [
+    ChatService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
